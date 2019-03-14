8d6d9a8616d68ea1a477cb570f81a4cfa20e8975
aqua25 <h.aqua25@gmail.com>
2019-03-15 01:06:43 +0900

    Inside controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,31 @@
 class TweetsController < ApplicationController
+
+  def index
+    @tweet = Tweet.new
+    @tweets = Tweet.includes(:user).order("created_at ASC")
+  end
+
+  def new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+      @tweet.save
+    redirect_to index: :action
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+
+
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:text,:image).merge(user_id: current_user.id)
+  end
 end
