4a26cf2d8e25ab38a6e54582e6a055236d4fe547
aqua25 <h.aqua25@gmail.com>
2019-03-15 01:37:46 +0900

    Inside models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,15 @@
 class Group < ApplicationRecord
+  has_many :messages
+  has_many :user_groups
+  has_many :users, through: :user_groups
+  validates :name, presence: true
+
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.text? ? last_message.text : "Have a image."
+    else
+      "No message yet."
+    end
+  end
 end
