d5e812568cb9bf255d618338750a1c1b9ff7328d
aqua25 <h.aqua25@gmail.com>
2019-03-15 00:41:52 +0900

    Routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "groups#index"
+  resources :users
+  resources :accounts
+  resources :articles
+  resources :tweets
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
